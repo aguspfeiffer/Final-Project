fded441c8b722d5d799499948af7c0d521548723
Agustin Pfeiffer <94919591+aguspfeiffer@users.noreply.github.com>
2022-06-02 10:14:30 -0300

    Datatable Updated

--- FinalProject/force-app/main/default/lwc/assignUserRoles/assignUserRoles.js
+++ FinalProject/force-app/main/default/lwc/assignUserRoles/assignUserRoles.js
@@ -1,22 +1,34 @@
 import { api, LightningElement, wire } from 'lwc';
 import getUsersByRole from '@salesforce/apex/ProjectDataService.getUsersByRole';
 
+
+const columns = [
+    { label: 'Resource', fieldName: 'Name' },
+    { label: 'Rate Per Hour', fieldName: 'RatePerHour__c', type: 'currency' },
+    { label: 'StartDate', fieldName: 'StartDateResource__c', type: 'date', editable : true },
+    { label: 'EndDate', fieldName: 'EndDateResource__c', type: 'date', editable : true },
+    { label: 'SquadLead', fieldName: 'SquadLead__c', type: 'boolean', editable: true}
+];
+
 export default class AssignUserRoles extends LightningElement {
 
-    @api 
-    roles;
+    columns = columns;
+    @api roles;
     @api recordId;
-    
     users
     
     @wire(getUsersByRole, {role:'$roles', projectId:'$recordId'})
     wiredUsers ({error, data}){
         if(data){
             this.users = data
-            console.log('Users: ' + JSON.stringify(data))
+            console.log('USERS-->', this.users);
         }
         else if(error){
             console.log(error)
         }
+    }
+
+    handleSave(event){
+        console.log('DraftValues-->', JSON.stringify(event.detail.draftValues));
     }
 }
